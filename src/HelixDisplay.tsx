d8a24284544f092f53af513d815c9f5ea78c2ba9
Jonathan Moore <programjm@gmail.com>
2019-12-11 02:28:09 -0600

    Add basic grid

--- src/HelixDisplay.tsx
+++ src/HelixDisplay.tsx
@@ -53,6 +53,35 @@ const Panel = styled.div<{ index: number }>`
              translateZ(calc(${translateFromCenter_putInCalc}));
 `;
 
+const gridSize = '2500px';
+const gridSquareSize = '100px';
+const gridLargeSquareSize = `calc(${gridSquareSize} * 5)`;
+
+const Grid = styled.div`
+  position: absolute;
+  left: calc(-1 * ${gridSize});
+  right: calc(-1 * ${gridSize});
+  top: calc(-1 * ${gridSize});
+  bottom: calc(-1 * ${gridSize});
+  overflow: visible;
+
+  transform: rotateX(90deg)   // Tilt 90 degrees so that it's perpendicular to the panels
+             translateZ(calc( -1 * ${panelHeight} * 0.6)) // Push it down under the lowest panel
+             rotateZ(17deg);  // Rotate it a bit so that the grid lines don't line up with the first panel 
+  
+  background-size: 100% 100%, // size for distance fading overlay
+                   ${gridSquareSize} ${gridSquareSize}, // Sizes for the horizonal & vertical thin lines
+                   ${gridSquareSize} ${gridSquareSize}, 
+                   ${gridLargeSquareSize} ${gridLargeSquareSize}, // Sizes for the horizontal & vertical thick lines
+                   ${gridLargeSquareSize} ${gridLargeSquareSize};
+  background-image: radial-gradient(circle, transparent 0%, white 90%), // distance fade overlay
+                    linear-gradient(to right, #818181 1px, transparent 1px), // thin horizontal lines
+                    linear-gradient(to bottom, #818181 1px, transparent 1px), // thin vertical lines
+                    linear-gradient(to right, #585858 4px, transparent 1px), // thick horizontal lines
+                    linear-gradient(to bottom, #585858 4px, transparent 1px); // thick vertical lines
+`;
+
+
 
 export const HelixDisplay = ({children}: PropsWithChildren<{}>) => {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -71,6 +100,7 @@ export const HelixDisplay = ({children}: PropsWithChildren<{}>) => {
             <KeyboardEventHandler handleKeys={['right']} onKeyEvent={goRight}/>
             <Scene>
                 <Helix currentIndex={currentIndex}>
+                    <Grid />
                     {React.Children.toArray(children).map((content, index) =>
                         <Panel index={index} key={index}>{content}</Panel>
                     )}
