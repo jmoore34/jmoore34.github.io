1b86c9fddece4c324b894f9c022776aa4c0530e9
Jonathan Moore <programjm@gmail.com>
2019-12-10 22:56:03 -0600

    Basic 3D carousel rendering functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const panelContents = [<h1>1</h1>,<h1>2</h1>,<h1>3</h1>,<h1>4</h1>,<h1>5</h1>,<h1>6</h1>,<h1>7</h1>,<h1>8</h1>];
+
+const panelWidth = "50vw";
+const panelHeight = "50vh";
+const panelMargin = "14px";
+const totalPanels = panelContents.length;
+const angularWidth_rads = 2 * Math.PI / totalPanels; // the total angular width of each panel, in radians
+const translateFromCenter_putInCalc = `0.5 * ${panelWidth} / ${Math.tan(angularWidth_rads/2)}`;
+// how much each panel should be translated from the y axis. NOTE: should be placed within a calc() expression
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Scene = styled.div`
+  width: ${panelWidth};
+  height: ${panelHeight};
+  border: 1px #a3a3a3 dashed;
+  perspective: 1000px;
+`;
+
+const Helix = styled.div<{currentIndex: number}>`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  transform-style: preserve-3d;
+  transition: transform 1s;
+  border: 1px red dashed;
+  transform: translateZ(calc( -1 * ${translateFromCenter_putInCalc}))
+             rotateY(${props => -1 * props.currentIndex * angularWidth_rads}rad); 
+`;
+
+const Panel = styled.div<{index: number}>`
+  position: absolute;
+  left: ${panelMargin};
+  right: ${panelMargin};
+  top: ${panelMargin};
+  bottom: ${panelMargin};
+  background-color: hsla(0deg,100%,100%,0.6);
+  border: 5px hsla(${props => props.index*angularWidth_rads}rad, 90%, 50%, 0.9) solid;
+  
+  transform: rotateY(${props => props.index*angularWidth_rads}rad)
+             translateZ(calc(${translateFromCenter_putInCalc}));
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Scene>
+        <Helix currentIndex={3}>
+            {panelContents.map( (content, index) =>
+                <Panel index={index} key={index}>{content}</Panel>
+            )}
+        </Helix>
+      </Scene>
+    </Container>
   );
-}
+};
 
 export default App;
