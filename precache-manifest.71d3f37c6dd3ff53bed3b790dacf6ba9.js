32d142c80c4382331554d398d11aa55a394fc2f9
Jonathan Moore <jtm170330@utdallas.edu>
2020-11-06 21:18:25 -0600

    Updates

---
+++ precache-manifest.71d3f37c6dd3ff53bed3b790dacf6ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a399e60f1438ef70783127bd2354db2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7010230291d209f25b07",
+    "url": "./static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "acd2c8e4c89be3bccf11",
+    "url": "./static/js/2.0dc96291.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "./static/js/2.0dc96291.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7010230291d209f25b07",
+    "url": "./static/js/main.17cb2ee6.chunk.js"
+  },
+  {
+    "revision": "dec3f7e5f2f3c7a384e2",
+    "url": "./static/js/runtime-main.ad95abcd.js"
+  }
+]);
--- readme.md
+++
@@ -1,6 +0,0 @@
-# Portfolio
-This is my 3D-helix portfolio, viewable live at https://jmoore34.github.io/.
-
-#### Build note
-
-GitHub requires personal pages to be deployed from master; hence, source files are located in the source branch, and gh-pages has been configured to build to master.
