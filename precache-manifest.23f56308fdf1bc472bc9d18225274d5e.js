7f2e23e185c519cdee4a6328e9a73091d89465ab
Jonathan Moore <jonm@duck.com>
2023-12-10 14:37:12 -0600

    Updates

---
+++ precache-manifest.23f56308fdf1bc472bc9d18225274d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076b88b60801611c48006a1065a8809b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d40b1b6ec9775c879579",
+    "url": "./static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "b104e64c9f78c0d1624d",
+    "url": "./static/js/2.62a5f66b.chunk.js"
+  },
+  {
+    "revision": "eecd5e993fbe5ad1378b3b4636839fc9",
+    "url": "./static/js/2.62a5f66b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d40b1b6ec9775c879579",
+    "url": "./static/js/main.00187568.chunk.js"
+  },
+  {
+    "revision": "e03bf809b7610633a482",
+    "url": "./static/js/runtime-main.3c22df83.js"
+  }
+]);
